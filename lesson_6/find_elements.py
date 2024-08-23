4e8776ee4d0dd6c813051685f524431c0e6eb145
Atorami <atoramiseo@mail.ru>
2024-08-23 14:00:33 +0200

    feat: add lesson 6

---
+++ lesson_6/find_elements.py
@@ -0,0 +1,14 @@
+import time
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://hyperskill.org/courses")
+driver.find_elements("class name", "nav-link")[3].click()
+
+time.sleep(5)
